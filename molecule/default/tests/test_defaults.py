95d21e46067438c0d9b92765a7ae21561c207f4b
Timothy Lytle <timothy.lytle@miarec.com>
2023-11-21 08:07:50 -0700

    use sox to convert audio files as a means to test supported formats

--- molecule/default/tests/test_defaults.py
+++ molecule/default/tests/test_defaults.py
@@ -26,15 +26,31 @@ def test_bins(host):
         assert b.exists
         assert b.is_file
 
+# def test_supported_formats(host):
+
+#     help_output = host.run('/usr/local/bin/sox --help | grep "AUDIO FILE FORMATS:"')
+
+#     assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
+
+#     # parse the supported formats into a list
+#     supported_formats = help_output.stdout.split(":")[1].split()
+
+#     assert "mp3" in supported_formats
+#     # assert "ogg" in supported_formats
+#     assert "wav" in supported_formats
+
+
 def test_supported_formats(host):
+    # Define list of formats
+    formats = [
+        "mp3",
+        "wav"
+        # "ogg"
+    ]
 
-    help_output = host.run('/usr/local/bin/sox --help | grep "AUDIO FILE FORMATS:"')
+    # Generate a file to be converted
+    generated_file = host.run('sox -n -r 16000 sample.wav synth 3 sine 500')
 
-    assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
-
-    # parse the supported formats into a list
-    supported_formats = help_output.stdout.split(":")[1].split()
-
-    assert "mp3" in supported_formats
-    # assert "ogg" in supported_formats
-    assert "wav" in supported_formats
+    for format in formats:
+        c = host.run('sox -r 8000 sample.wav output.{}'.format(format))
+        assert c.succeeded
