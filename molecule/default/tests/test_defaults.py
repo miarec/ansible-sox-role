6551bf486f29a4300dc65eea8c96e3abb326c912
Timothy Lytle <timothy.lytle@miarec.com>
2023-12-11 13:45:52 -0700

    code cleanup

--- molecule/default/tests/test_defaults.py
+++ molecule/default/tests/test_defaults.py
@@ -4,18 +4,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-# def test_libs(host):
-#     libs = [
-#         "/usr/local/lib/libmp3lame.a",
-#         "/usr/local/lib/libmad.a",
-#         "/usr/local/lib/libogg.a",
-#     ]
-
-#     for lib in libs:
-#         f = host.file(lib)
-#         assert f.exists
-#         assert f.is_file
-
 def test_bins(host):
     bins = [
         "/usr/local/bin/sox"
@@ -26,31 +14,14 @@ def test_bins(host):
         assert b.exists
         assert b.is_file
 
-# def test_supported_formats(host):
-
-#     help_output = host.run('/usr/local/bin/sox --help | grep "AUDIO FILE FORMATS:"')
-
-#     assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
-
-#     # parse the supported formats into a list
-#     supported_formats = help_output.stdout.split(":")[1].split()
-
-#     assert "mp3" in supported_formats
-#     # assert "ogg" in supported_formats
-#     assert "wav" in supported_formats
-
-
 def test_supported_formats(host):
-    # Define list of formats
-    formats = [
-        "mp3",
-        "wav"
-        # "ogg"
-    ]
 
-    # Generate a file to be converted
-    generated_file = host.run('sox -n -r 16000 sample.wav synth 3 sine 500')
+    help_output = host.run('/usr/local/bin/sox --help | grep "AUDIO FILE FORMATS:"')
 
-    for format in formats:
-        c = host.run('sox -r 8000 sample.wav output.{}'.format(format))
-        assert c.succeeded
+    assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
+
+    # parse the supported formats into a list
+    supported_formats = help_output.stdout.split(":")[1].split()
+
+    assert "mp3" in supported_formats
+    assert "wav" in supported_formats
--- molecule/install-from-package/tests/test_defaults.py
+++ molecule/install-from-package/tests/test_defaults.py
@@ -4,26 +4,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-# def test_libs(host):
-#     if host.system_info.distribution == "centos":
-#         libs = [
-#             "/lib64/libogg.so.0",
-#             "/lib64/libmad.so.0",
-#             "/lib64/libmp3lame.so.0"
-#         ]
-
-#     if host.system_info.distribution == "ubuntu":
-#         libs = [
-#             "/lib/x86_64-linux-gnu/libogg.so.0",
-#             "/lib/x86_64-linux-gnu/libmad.so.0",
-#             "/lib/x86_64-linux-gnu/libmp3lame.so.0"
-#         ]
-
-#     for lib in libs:
-#         f = host.file(lib)
-#         assert f.exists
-#         assert f.is_file
-
 def test_bins(host):
     bins = [
         "/usr/bin/sox"
@@ -34,32 +14,15 @@ def test_bins(host):
         assert b.exists
         assert b.is_file
 
-# This is probably fin if the
-# def test_supported_formats(host):
-
-#     help_output = host.run('/usr/bin/sox --help | grep "AUDIO FILE FORMATS:"')
-
-#     assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
-
-#     # parse the supported formats into a list
-#     supported_formats = help_output.stdout.split(":")[1].split()
-
-#     assert "mp3" in supported_formats
-#     assert "ogg" in supported_formats
-#     assert "wav" in supported_formats
-
 def test_supported_formats(host):
-    # Define list of formats
-    # make this conditional based on OS?
-    formats = [
-        "mp3",
-        "wav",
-        "ogg"
-    ]
 
-    # Generate a file to be converted
-    generated_file = host.run('sox -n -r 16000 sample.wav synth 3 sine 500')
+    help_output = host.run('/usr/bin/sox --help | grep "AUDIO FILE FORMATS:"')
 
-    for format in formats:
-        c = host.run('sox -r 8000 sample.wav output.{}'.format(format))
-        assert c.succeeded
+    assert help_output.rc == 0 , "AUDIO_FILE_FORMATS is missing in sox --help output"
+
+    # parse the supported formats into a list
+    supported_formats = help_output.stdout.split(":")[1].split()
+
+    assert "mp3" in supported_formats
+    assert "ogg" in supported_formats
+    assert "wav" in supported_formats
